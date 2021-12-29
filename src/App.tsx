6e571be7be037fc0483b6d61104616ebf280e841
Lucas Lopes <dev.thuram@gmail.com>
2021-12-29 20:13:02 -0300

    refact: remove all code from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 import { Layout } from './components/Layout'
-import { getAllSuggestions } from './services/firebase'
-import { SuggestionData } from './models/Suggestion'
 import { SuggestionList } from './components/SuggestionList'
 import { SuggestionItem } from './components/SuggestionItem'
-import { Loading } from './components/Loading'
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true)
-  const [suggestions, setSuggestions] = useState<SuggestionData[]>([])
-
-  useEffect(() => {
-    getAllSuggestions().then((data) => {
-      setSuggestions(data)
-      setIsLoading(false)
-    })
-  }, [])
-
   return (
     <Layout>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <SuggestionList>
-          {suggestions.map((suggestion) => (
-            <SuggestionItem key={suggestion.id} suggestion={suggestion} />
-          ))}
-        </SuggestionList>
-      )}
+      <SuggestionList>
+        <SuggestionItem />
+      </SuggestionList>
     </Layout>
   )
 }
--- src/components/SuggestionItem/index.tsx
+++ src/components/SuggestionItem/index.tsx
@@ -1,29 +1,19 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { FiThumbsUp as VoteIcon } from 'react-icons/fi'
-import { SuggestionData } from '../../models/Suggestion'
 
-type SuggeestionItemProps = {
-  suggestion: SuggestionData
-}
-
-export const SuggestionItem = ({ suggestion }: SuggeestionItemProps) => {
-  const handleVote = () => {
-    // const { votes } = suggestion
-  }
-
+export const SuggestionItem = () => {
   return (
     <section className="flex text-white mt-4">
       <button
-        onClick={handleVote}
         type="button"
         className="flex flex-col justify-center items-center w-24 h-24 p-5 border-2 border-slate-500 rounded mr-4 hover:bg-slate-500"
       >
         <VoteIcon size={24} />
-        <span className="text-lg">{suggestion.votes}</span>
+        <span className="text-lg">10</span>
       </button>
       <div className="flex flex-col justify-center">
-        <h2 className="text-xl text-fuchsia-500">{suggestion.title}</h2>
-        <p>{suggestion.description}</p>
+        <h2 className="text-xl text-fuchsia-500">Curso de HTML e CSS</h2>
+        <p>Criar um curso para iniciantes no FrontEnd</p>
       </div>
     </section>
   )
